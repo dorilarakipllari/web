6fef4ba7e24e439903fcf4f6b6ea3dfed71334ce
Dorila Rakipllari <dorirakipllari@gmail.com>
2025-05-23 14:06:34 +0200

    Chapter 4

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 
 
 app.use('/api/tasks', require('./routes/taskRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server listening on ${port}`));
