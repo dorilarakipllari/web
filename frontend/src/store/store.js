6df9dbd14c0c04379d9b0f06bd25d23e558c1fe0
Dorila Rakipllari <dorirakipllari@gmail.com>
2025-06-05 00:38:21 +0200

    Chapter 9

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+
 import { userApi } from './apis/userApis';
+import { taskApi } from './apis/taskApi';
+
 export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        [userApi.reducerPath]: userApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat(userApi.middleware),
+  reducer: {
+    user: userReducer,
+    [userApi.reducerPath]: userApi.reducer,
+    [taskApi.reducerPath]: taskApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(userApi.middleware)
+      .concat(taskApi.middleware),
 });
