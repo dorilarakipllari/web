d183228a83383810988118c09abca591a5e4a8a4
Dorila Rakipllari <dorirakipllari@gmail.com>
2025-06-05 00:08:17 +0200

    Chapter 8

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,53 @@
 import { FaSignInAlt } from 'react-icons/fa'
-import { useState } from "react"
+
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../store/slices/userSlice';
+import { useLoginMutation } from '../store/apis/userApis';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 const Login = () => {
     const [formData, setFormData] = useState({ email: '', password: '' })
     const { email, password } = formData;
 
-    const onChange = e => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [login, { isLoading }] = useLoginMutation();
+    const user = useSelector(state => state.user);
+
+    const onChange = (e) => {
         setFormData(prevState => ({
             ...prevState,
             [e.target.name]: e.target.value
         }))
     }
-    const onSubmit = e => {
-        e.preventDefault()
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await login(formData);
+            if (response.error) {
+                toast.error(response.error.data?.message || response.error.error || 'Registration failed');
+            } else {
+                dispatch(setUser(response.data));
+                localStorage.setItem('user', JSON.stringify(response.data))
+                navigate('/');
+                toast.success(`Welcome ${response.data.name}!`);
+
+            }
+        } catch (err) {
+            console.error('Login failed', err);
+        }
     }
+
+    useEffect(() => {
+        if (user) {
+            navigate('/');
+        }
+    }, [user, navigate]);
+
     return (
         <>
             <section className='heading'>
@@ -23,35 +58,22 @@ const Login = () => {
             <section className='form'>
                 <form onSubmit={onSubmit}>
                     <div className='form-group'>
-                        <input
-                            type='email'
-                            className='form-control'
-                            id='email'
-                            name='email'
-                            value={email}
-                            placeholder='Enter your email'
-                            onChange={onChange}
+                        <input required type='email' className='form-control' id='email' name='email' value={email} placeholder='Enter your email' onChange={onChange}
                         />
                     </div>
-
                     <div className='form-group'>
-                        <input
-                            type='password'
-                            className='form-control'
-                            id='password'
-                            name='password'
-                            value={password}
-                            placeholder='Enter password'
-                            onChange={onChange}
+                        <input required type='password' className='form-control' id='password' name='password' value={password} placeholder='Enter password' onChange={onChange}
                         />
                     </div>
-
                     <div className='form-group'>
-                        <button type='submit' className='btn btn-block'>Submit</button>
+                        <button type='submit' disabled={isLoading} className='btn btn-block'>
+                            {isLoading ? "Please Wait..." : "Login"}
+                        </button>
                     </div>
                 </form>
             </section>
         </>
-    );
+    )
 }
+
 export default Login
